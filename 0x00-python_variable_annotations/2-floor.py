45a0c623061718cc8a2c73f7c2fd57fe68b5642d
cristian gomez <1154@holbertonschool.com>
2021-07-27 16:49:00 -0400

    [add] task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotation (floor)"""
+import math
+
+
+def floor(n: float) -> int:
+    """concat strings
+
+    Args:
+    -> n is a float to find a floor
+
+    returns
+    -> the floor of the number
+    """
+    return math.floor(n)
