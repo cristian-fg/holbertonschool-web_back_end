85791ebd3dd819786780500a4a3b6e85ac8880bb
cristian gomez <1154@holbertonschool.com>
2021-07-27 16:53:23 -0400

    [add] task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotation (to string)"""
+import math
+
+
+def to_str(n: float) -> str:
+    """convert to string
+
+    Args:
+    -> n is a float to convert
+
+    returns
+    -> the convert string
+    """
+    return str(n)
