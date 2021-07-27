f9f0ecd379f17ab1b6146b12021335e35a6fd397
cristian gomez <1154@holbertonschool.com>
2021-07-27 16:43:42 -0400

    [add] task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotation (concat)"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat strings
+
+    Args:
+    -> str1 is a string to concat
+    -> str2 is a string to concat
+
+    returns
+    -> the concat of str1 and str2
+    """
+    return str1 + str2
