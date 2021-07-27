7c3297319ff8a1eb8f205f3a24a6892778a7afd2
cristian gomez <1154@holbertonschool.com>
2021-07-27 16:39:31 -0400

    [add] task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotation (add)"""
+
+
+def add(a: float, b: float) -> float:
+    """add floats numbers
+
+    Args:
+    -> a is a float number
+    -> b is a float number
+
+    returns
+    -> the add of a and b
+    """
+    return a + b
