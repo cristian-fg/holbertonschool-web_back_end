9a8d79b79a5a4eb516642bca5635dacb8d453b67
cristian gomez <1154@holbertonschool.com>
2021-07-28 12:31:38 -0400

    [add] task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """multiplier number
+    """multiplier float number
 
     Args:
     -> multiplier a float
