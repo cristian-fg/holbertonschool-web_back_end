c7165af6021c88ddbe7aeae2c036d0d3a7448214
cristian gomez <1154@holbertonschool.com>
2021-07-28 13:16:21 -0400

    [add] main of tasks

---
+++ 0x00-python_variable_annotations/main/9-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+element_length =  __import__('9-element_length').element_length
+
+i = element_length([[1, 3, 4, 5], [3, 6, 7]])
+print(i)
+print(element_length.__annotations__)
