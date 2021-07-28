91f058a2a23d1cc725afc0fe9e85851121703ed8
cristian gomez <1154@holbertonschool.com>
2021-07-28 12:07:04 -0400

    [add] task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Complex types (functions)"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """multiplier number
+
+    Args:
+    -> multiplier a float
+
+    returns
+    -> a function callable
+    """
+    def func(n: float) -> float:
+        return n * n
+
+    return func
