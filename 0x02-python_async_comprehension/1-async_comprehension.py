96bd749e4714e6ad37aa01f8f2427f87ca6e79ff
cristian gomez <1154@holbertonschool.com>
2021-09-09 11:27:38 -0400

    [add] task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 """Async Comprehensions"""
 
 async_generator = __import__('0-async_generator').async_generator
-from typing import List, Generator
+from typing import List
 
 
-async def async_comprehension() -> List[Generator[float, None, None]]:
+async def async_comprehension() -> List[float]:
     """async_comprehension function"""
     data = [i async for i in async_generator()]
 
