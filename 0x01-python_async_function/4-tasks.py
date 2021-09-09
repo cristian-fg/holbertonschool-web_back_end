1fdecbbe4bd2642df8165988ecb528a45fd491df
cristian gomez <1154@holbertonschool.com>
2021-09-09 10:50:52 -0400

    [add] task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ multiple coroutines at the same time with async"""
+
+    r = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+
+    return sorted(r)
