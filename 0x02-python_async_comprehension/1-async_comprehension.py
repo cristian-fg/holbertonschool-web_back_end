e104fb806b5d88b18eb3381d72b3f38483feb610
cristian gomez <1154@holbertonschool.com>
2021-09-09 11:24:02 -0400

    [add] task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """async_comprehension function"""
+    data = [i async for i in async_generator()]
+
+    return data
