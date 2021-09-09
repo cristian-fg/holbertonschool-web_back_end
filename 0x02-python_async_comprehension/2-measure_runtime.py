28de26f137286d783a751187880ea159bf144736
cristian gomez <1154@holbertonschool.com>
2021-09-09 11:55:26 -0400

    [add] task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,7 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure time"""
     s = time.time()
-    await asyncio.gather(*(async_comprehension() for i in range(3)))
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     elapsed = time.time() - s
 
     return elapsed
