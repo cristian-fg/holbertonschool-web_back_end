6ebffd23de10664c7d40b443938bab32bbc576e4
cristian gomez <1154@holbertonschool.com>
2021-09-09 13:13:16 -0400

    [add] task 0

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Basic dictionary
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class BasicCahe"""
+
+    def put(self, key, value):
+        """Put a key/value pair into the cache."""
+        if key is not None or value is not None:
+            self.cache_data[key] = value
+
+    def get(self, key):
+        """get a key/value pair from the cache"""
+        if key is not None:
+            key_get = self.cache_data.get(key)
+            return key_get
