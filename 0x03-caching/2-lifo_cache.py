a327910c1680c8cba6c1d799ce1ae2eb705e73ff
cristian gomez <1154@holbertonschool.com>
2021-09-10 11:16:11 -0400

    [add] task 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" LIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO cache"""
+
+    def __init__(self):
+        super().__init__()
+        self.last_key = []
+
+    def put(self, key, item):
+        """Add a item in the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+
+                if key in self.cache_data:
+                    self.cache_data[key] = item
+                    self.last_key.append(key)
+                else:
+                    self.cache_data.pop(self.last_key[-1])
+                    print(f"DISCARD: {self.last_key[-1]}")
+                    self.cache_data[key] = item
+                    self.last_key.append(key)
+
+            self.cache_data[key] = item
+            self.last_key.append(key)
+
+    def get(self, key):
+        """get a item from the cache"""
+        if key is not None:
+            get_key = self.cache_data.get(key, None)
+            return get_key
